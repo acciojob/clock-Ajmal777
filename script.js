0e6d1230923778f4c11e1a72fde66d70e19d6d46
Ajmal777 <81432526+Ajmal777@users.noreply.github.com>
2023-06-20 18:35:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const timer = document.getElementById('timer');
 const date = new Date();
 let hour, minute, seconds;
-let AMPM = 'AM';
-setInterval{()=>{
+setInterval(()=>{
 	hour = date.getHours();
 	AMPM = hour >= 12 ? 'PM' : 'AM';
 	if(hour == 24) hour = '00';
 	else if(hour > 12) hour = hour - 12;
 	timer.innerText = `${date.getMonth()}/${date.getDate()}/${date.getFullYear()}, ${hour}:${date.getMinutes()}:${date.getSeconds()} ${AMPM}`;
-}, 1000};
+}, 1000);
